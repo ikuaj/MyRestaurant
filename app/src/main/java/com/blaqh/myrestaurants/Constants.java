805b6d7a754ac645961b87917c29c50cf008b1dc
blaqh <ikuaj96@gmail.com>
2018-06-14 16:32:58 +0300

    All Files

---
+++ app/src/main/java/com/blaqh/myrestaurants/Constants.java
@@ -0,0 +1,7 @@
+package com.blaqh.myrestaurants;
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
